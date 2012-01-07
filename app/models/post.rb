6274fd333f09646b716eb88afb358feaa7d69698
Fayimora Femi-Balogun <fayimora@hotmail.co.uk>
2012-01-07 08:18:32 +0000

    added comment functionality
    added comment functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
-
+    validates :name, :presence => true #name must be present
+    validates :body, :presence => true #name must be present
+    validates :title, :presence => true, #title must be present
+                  :length => {:minimum => 5} #title must have minimum length of 5
+    has_many :comments 
 end
