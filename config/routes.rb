98be1f523a5121ec17593f955a54902cf23ebb77
Samuel Cochran <sj26@sj26.com>
2012-01-07 12:24:53 +0800

    Use RESTful routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Blog::Application.routes.draw do
-  root :to => 'posts#show'
-  match 'posts' => 'posts#show'
-  get "posts/new"
-  get "posts/edit"
-  get "posts/show"
-  get "posts/delete"
-  # get "posts/create"
+  resources :posts
+  root :to => redirect('/posts')
 end
